088ce4bb6c06de78cc26ea1c3b725caca51e440c
danielpgauer <danielpgauer@gmail.com>
2019-07-26 07:08:26 -0300

    fix react-navigation didFocus

--- BackHandler.android.js
+++ BackHandler.android.js
@@ -3,28 +3,41 @@ import { withNavigation } from 'react-navigation';
 import { BackHandler } from 'react-native';
 
 class BackHandlerAndroid extends React.Component {
-  _willFocusSubscription;
+  _didFocusSubscription;
   _willBlurSubscription;
 
   constructor(props) {
     super(props);
-    this._willFocusSubscription = props.navigation.addListener('willFocus', payload =>
-      BackHandler.addEventListener('hardwareBackPress', this.onBackPressed)
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPressed)
+    this._didFocusSubscription = props.navigation.addListener('didFocus', payload => {
+        BackHandler.removeEventListener('hardwareBackPress', this.onBackPressed)
+        BackHandler.addEventListener('hardwareBackPress', this.onBackPressed)
+        if (props.name) {
+          console.log('didFocus', props.name)
+        }
+      }
     );
   }
 
   componentDidMount() {
-    this._willBlurSubscription = this.props.navigation.addListener('willBlur', payload =>
-      BackHandler.removeEventListener('hardwareBackPress', this.onBackPressed)
+    this._willBlurSubscription = this.props.navigation.addListener('willBlur', payload => {
+        BackHandler.removeEventListener('hardwareBackPress', this.onBackPressed)
+        if (this.props.name) {
+          console.log('willBlur', this.props.name)
+        }
+      }
     );
   }
 
   onBackPressed = () => {
+    if (this.props.name) {
+      console.log('onBackPressed', this.props.name)
+    }
     return this.props.onBackPress();
   };
 
   componentWillUnmount() {
-    this._willFocusSubscription && this._willFocusSubscription.remove();
+    this._didFocusSubscription && this._didFocusSubscription.remove();
     this._willBlurSubscription && this._willBlurSubscription.remove();
     BackHandler.removeEventListener('hardwareBackPress', this.onBackPressed);
   }
@@ -34,4 +47,4 @@ class BackHandlerAndroid extends React.Component {
   }
 }
 
-export const AndroidBackHandler = withNavigation(BackHandlerAndroid);
+export const AndroidBackHandler = BackHandlerAndroid;
